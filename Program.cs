b227489c921404ebe82202ce05413269d06e0e3b
Thinh Truong <thinh2it@gmail.com>
2023-08-15 09:47:46 +0700

    Add SetLights function

--- Program.cs
+++ Program.cs
@@ -3,104 +3,44 @@
 
 public partial class Program
 {
+    // Traffic light periods
+    private TimeSpan period1 = TimeSpan.FromSeconds(30);
+    private TimeSpan period2 = TimeSpan.FromSeconds(30);
+    
+    // Car arrival rates (cars per minute)
+    private int arrivalRateNorth = 10;
+    private int arrivalRateSouth = 5;
+    private int arrivalRateEast = 15;
+    private int arrivalRateWest = 8;
+
+    // Time for each car to clear the intersection
+    private int clearanceTime = 2;
+
     public static Queue<Car> northQueue = new Queue<Car>();
     public static Queue<Car> southQueue = new Queue<Car>();
     public static Queue<Car> westQueue = new Queue<Car>();
     public static Queue<Car> eastQueue = new Queue<Car>();
 
-    private static bool _northSouthTurn = true;
+    private static bool lightNorth = false;
+    private static bool lightSouth = false;
+    private static bool lightWest = false;
+    private static bool lightEast = false;
 
     static void Main(string[] args)
     {
+        SetLights(true, true, false, false);
+    }
 
-        var enqueueTask = new Task(() => {
-            while (true)
-            {
-                northQueue.Enqueue(new Car());
-                southQueue.Enqueue(new Car());
-                westQueue.Enqueue(new Car());
-                eastQueue.Enqueue(new Car());
-                Thread.Sleep(6000);
-            }
-        }, TaskCreationOptions.LongRunning);
-        var changeGreenLightTask = new Task(() =>
-        {
-            while (true)
-            {
-                var greenLightObservable = new GreenLightToggleObservable();
-                var greenLightObserver = new GreenLightObserver(greenLightObservable);
-                greenLightObservable.GreenLight = new GreenLightIndicator
-                {
-                    IsTurnOn = !_northSouthTurn
-                };
-                _northSouthTurn = !_northSouthTurn;
-                Thread.Sleep(3000);
-            }
-        }, TaskCreationOptions.LongRunning);
-
-        enqueueTask.Start();
-        changeGreenLightTask.Start();
-
-        Console.ReadKey();
+    public static void SetLights(bool north, bool south, bool west, bool east)
+    {
+        lightNorth = north;
+        lightSouth = south;
+        lightWest = west;
+        lightEast = east;
     }
 
     public class Car
     {
-        public int LeavingTime { get; set; }
-    }
-
-    public class GreenLightIndicator
-    {
-        public bool IsTurnOn { get; set; }
-    }
-
-    public class GreenLightObserver
-    {
-        public GreenLightObserver(GreenLightToggleObservable greenLightObservable)
-        {
-            greenLightObservable.UpdateGreenLightEvent += GreenLightStatusChanged;
-        }
-
-        private void GreenLightStatusChanged(object sender, UpdateGreenLightEventArgs e)
-        {
-            if (_northSouthTurn)
-            {
-                northQueue.Dequeue();
-                southQueue.Dequeue();
-            }
-            else
-            {
-                westQueue.Dequeue();
-                eastQueue.Dequeue();
-            }
-        }
-    }
-
-    public class GreenLightToggleObservable
-    {
-        private GreenLightIndicator greenLight;
-
-        public event EventHandler<UpdateGreenLightEventArgs> UpdateGreenLightEvent = delegate { };
-
-        public GreenLightIndicator GreenLight
-        {
-            get => greenLight;
-            set
-            {
-                greenLight = value;
-                UpdateGreenLightEvent(this, new UpdateGreenLightEventArgs(greenLight));
-            }
-        }
-    }
-
-    public class UpdateGreenLightEventArgs : EventArgs
-    {
-        public UpdateGreenLightEventArgs(GreenLightIndicator greenLight)
-        {
-            GreenLightIndicator = greenLight;
-        }
-
-        public GreenLightIndicator GreenLightIndicator { get; set; }
     }
 }
 
